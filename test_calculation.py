6f45e6d0329373c80d24c11132921a4096d4c7e8
Suyog Vyas <76203074+SuyogVyas@users.noreply.github.com>
2024-06-09 18:50:05 +0530

    Add files via upload

---
+++ test_calculation.py
@@ -0,0 +1,13 @@
+import pytest
+from calc import add,subtract
+
+def test_add():
+    assert add(3,4) == 7
+    assert add(8,11) == 19
+    assert add(8,4) == 33
+
+
+def test_subtract():
+    assert subtract(4,2) == 2
+    assert subtract(3,4) == 4
+    assert subtract(44,44) == 2
