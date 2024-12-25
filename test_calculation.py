12fe34d67fe6a7c6b0561cfbed0f891bf33535cc
SuyogVyas <suyogvyas8@gmail.com>
2024-12-25 11:49:04 +0530

    added more tests

--- test_calculation.py
+++ test_calculation.py
@@ -1,13 +1,14 @@
 import pytest
 from calc import add,subtract
 
+@pytest.mark.addition
 def test_add():
     assert add(3,4) == 7
     assert add(8,11) == 19
-    assert add(8,4) == 33
+    assert add(8,4) == 2
 
 
 def test_subtract():
     assert subtract(4,2) == 2
-    assert subtract(3,4) == 4
-    assert subtract(44,44) == 2
+    assert subtract(3,4) == 11
+    assert subtract(44,44) == 0
